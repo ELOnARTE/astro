8ab839802a42da4f655cc4e42b49ea0eb22b773f
elonarte <elonarte@yahoo.com>
2025-06-23 11:32:38 -0300

    commit26

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+// src/consts.js
+export const SITE_TITLE = 'ELOnARTE'; // <-- CORRIGIDO
+export const SITE_DESCRIPTION = 'Conectando Arte, Terapia & Tecnologia.';
