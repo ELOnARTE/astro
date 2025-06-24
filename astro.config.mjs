bc106b2ba3f4348c51a5b3f23f2d0852dc5a1107
elonarte <elonarte@yahoo.com>
2025-06-24 06:05:10 -0300

    commit43

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,13 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://elonart.pages.dev',
 	integrations: [mdx(), sitemap()],
+	
+// ADICIONE A SEÇÃO VITE ABAIXO
+  vite: {
+    build: {
+      // Define um limite para ativos embutidos (incluindo CSS). O valor é em bytes.
+      // 15kb = 15360 bytes. Isso garantirá que seus arquivos de CSS sejam embutidos.
+      assetsInlineLimit: 15360
+    }
+  }
 });
