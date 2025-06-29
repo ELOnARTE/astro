42b584794ac6cf3e6964fc9e153805d5ffa817e0
elonarte <elonarte@yahoo.com>
2025-06-29 15:41:45 -0300

    commit151

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://elonart.pages.dev',
+	site: 'https://elonarte.com',
 	integrations: [mdx(), sitemap()],
 	
 // ADICIONE A SEÇÃO VITE ABAIXO
